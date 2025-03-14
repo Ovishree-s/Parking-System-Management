039f031ddb1194171197cafece7e44cf20cae689
Ovishree <163629014+Ovishree-s@users.noreply.github.com>
2025-03-14 23:27:12 +0530

    Create parking-data.js

---
+++ AR Navigation  codes/parking-data.js
@@ -0,0 +1,16 @@
+    // Predefined virtual parking slots with (x, y) positions
+    const parkingSlots = {
+        "1": { "x": 0, "y": 0 },
+        "2": { "x": 2, "y": 0 },
+        "3": { "x": 4, "y": 0 },
+        "4": { "x": 6, "y": 0 },
+        "5": { "x": 8, "y": 0 },
+        "6": { "x": 10, "y": 0 },
+        "7": { "x": 12, "y": 0 },
+        "8": { "x": 14, "y": 0 },
+        "9": { "x": 16, "y": 0 },
+        "10": { "x": 18, "y": 0 }
+    };
+
+    // Export the data so it can be used in other files
+    export default parkingSlots;
